d5eb462346216b0da18535019646f2b38c1b7709
Prasad-D-Ware <prasadware42116@gmail.com>
2025-06-09 07:52:05 +0530

    ft : testing emits for transfer and approve

--- test/Token.t.sol
+++ test/Token.t.sol
@@ -7,6 +7,8 @@ import "src/Token.sol";
 
 contract TestToken is Test {
     Token t;
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 
     function setUp() public {
         t = new Token();
@@ -67,5 +69,30 @@ contract TestToken is Test {
             0x6b33AfEcce4198E5127d4c8970c16C1ed75Cc62a,
             30
         );
+    }
+
+    function testTransferEmit() public {
+        t.mint(address(this), 100);
+
+        vm.expectEmit(true, true, false, true);
+        emit Transfer(address(this),0x6b33AfEcce4198E5127d4c8970c16C1ed75Cc62a, 10);
+
+        t.transfer(0x6b33AfEcce4198E5127d4c8970c16C1ed75Cc62a, 10);
+    }
+
+    function testApproveAndTransferFromEmit() public {
+        t.mint(address(this), 100);
+
+        vm.expectEmit(true, true, false, true);
+        emit Approval(address(this), 0x6b33AfEcce4198E5127d4c8970c16C1ed75Cc62a, 50);
+
+        t.approve(0x6b33AfEcce4198E5127d4c8970c16C1ed75Cc62a, 50);
+
+        vm.expectEmit(true, true, false, true);
+        emit Transfer(address(this),0x6b33AfEcce4198E5127d4c8970c16C1ed75Cc62a, 10);
+
+        vm.prank(0x6b33AfEcce4198E5127d4c8970c16C1ed75Cc62a);
+        t.transferFrom(address(this), 0x6b33AfEcce4198E5127d4c8970c16C1ed75Cc62a, 10);
+
     }
 }
